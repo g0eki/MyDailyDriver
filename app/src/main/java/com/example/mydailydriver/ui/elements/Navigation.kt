f403ae32a73906629034c73293788f7e45d9fa0b
goeki <goeki.91@gmail.com>
2026-03-02 15:13:42 +0100

    Update

--- app/src/main/java/com/example/mydailydriver/ui/elements/Navigation.kt
+++ app/src/main/java/com/example/mydailydriver/ui/elements/Navigation.kt
@@ -1,18 +1,63 @@
 package com.example.mydailydriver.ui.elements
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.Navigation
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
 @Composable
 // fun Navigation(viewModel: MovieMakerViewModel = viewModel()) {
+//fun Navigation() {
+//    val navController = rememberNavController()
+//
+//    NavHost(navController = navController,
+//        startDestination = Screens.Start.name,
+//        modifier = Modifier) {
+//        composable(route= Screens.Start.name) {
+//            MainScreen()
+//        }
+//
+//        composable(route= Screens.Notes.name) {
+//            NoteScreen()
+//        }
+//    }
+//}
+
 fun Navigation() {
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = "start") {
-        composable("start") {
-            NoteScreen()
+    ModalNavigationDrawer(
+        modifier = Modifier.padding(8.dp),
+        drawerContent = {
+            ModalDrawerSheet {
+// NavigationDrawerItem ==> Über eine Funktion (For / Loop) Ordner oder Notien darstellen
+            }
+        },)
+    {
+        NavHost(navController = navController,
+            startDestination = Screens.Start.name,
+            modifier = Modifier) {
+            composable(route= Screens.Start.name) {
+                MainScreen()
+            }
+
+            composable(route= Screens.Notes.name) {
+                NoteScreen()
+                // Oder eine Neue Notiz soll ein Screen sein
+            }
         }
     }
+
 }
